b5c44d1d49b4080b16ba0b0a9b7cbec820cf19e9
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-03-03 19:19:02 +0300

    Add: Quote API added
    Quote API added and local quotes created.

--- logic/script.js
+++ logic/script.js
@@ -1,1 +1,22 @@
-console.log("Testing");
+let apiQuotes = [];
+
+// Show new function
+const new_quote = function () {
+  // Get a random quote from apiQuotes
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+};
+// Get Quotes from API
+async function get_quotes() {
+  const api_url = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(api_url);
+    apiQuotes = await response.json();
+    new_quote();
+  } catch (error) {
+    // Catch error here
+  }
+}
+
+// On Load
+get_quotes();
